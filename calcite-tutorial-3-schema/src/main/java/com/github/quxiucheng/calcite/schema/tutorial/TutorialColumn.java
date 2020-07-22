fddf9eaaf059c1ce3f35a31d5b979fccc6b9a5f3
songyx <songyx@knownsec.com>
2020-07-22 17:23:58 +0800

    first commit

---
+++ calcite-tutorial-3-schema/src/main/java/com/github/quxiucheng/calcite/schema/tutorial/TutorialColumn.java
@@ -0,0 +1,40 @@
+package com.github.quxiucheng.calcite.schema.tutorial;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.io.Serializable;
+
+/**
+ * 列信息
+ * @author quxiucheng
+ * @date 2019-04-26 11:18:00
+ */
+@Data
+public class TutorialColumn implements Serializable {
+    /**
+     * 字段名称
+     */
+    private String name;
+
+    /**
+     * 字段类型名称
+     */
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
